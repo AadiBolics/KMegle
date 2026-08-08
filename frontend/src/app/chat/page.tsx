093fec5909ec047dbb69da918905823067213a81
Aadinadhan <aadinadhanr24bcs0143@iiitkottayam.ac.in>
2026-08-09 01:33:22 +0530

    Added a relay turn server

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -46,7 +46,22 @@ export default function ChatDashboard() {
       { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
-      { urls: "stun:stun3.l.google.com:19302" },
+      { urls: "stun:openrelay.metered.ca:80" },
+      {
+        urls: "turn:openrelay.metered.ca:80",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
+      {
+        urls: "turn:openrelay.metered.ca:443?transport=tcp",
+        username: "openrelayproject",
+        credential: "openrelayproject",
+      },
     ],
   };
 
